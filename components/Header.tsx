79ce2fd0dfceb9a413ec1c7853cd1890ca1c7170
lleung030 <lucasleung2593@gmail.com>
2023-07-11 15:44:04 -0700

    framer-motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,29 @@
+'use client'
+import React from 'react';
 import { SocialIcon } from "react-social-icons";
-
+import { motion } from 'framer-motion';
+ 
 type Props = {};
 
 function Header() {
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 items-center">
+    
+      <motion.div
+      initial={{
+        x: -500,
+        opacity: 0,
+        scale: 0.5
+      }}
+      animate= {{
+        x:0,
+        opacity: 1,
+        scale: 1
+      }}
+      transition={{
+        duration: 1.5
+      }}
+       className="flex flex-row items-center">
         <SocialIcon
           url="https://youtube.com/"
           fgColor="gray"
@@ -21,19 +39,32 @@ function Header() {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+        </motion.div>
+      
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div 
+      initial={{
+        x: 500,
+        opacity: 0,
+        scale: 0.5,
+      }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        scale:1,
+      }}
+      transition={{duration:1.5}}
+      className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon
           className="cursor-pointer"
           network="email"
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
       <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
         Get in touch
       </p>
+      </motion.div>
     </header>
   );
 }
