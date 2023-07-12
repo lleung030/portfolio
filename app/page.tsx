12a643c18bf4c5205d1e6591e2c07f12de6e181f
lleung030 <lucasleung2593@gmail.com>
2023-07-12 09:29:40 -0700

    contact page front end

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import About from '@/components/About';
 import WorkExperience from '@/components/WorkExperience';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
+import ContactMe from '@/components/ContactMe';
 
 export default function Home() {
   return (
@@ -28,6 +29,9 @@ export default function Home() {
       </section>
       <section id='projects' className='snap-start'>
         <Projects />
+      </section>
+      <section id='contact' className='snap-start'>
+        <ContactMe />
       </section>
     </div>
   );
