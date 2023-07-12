9b948a1de568b6f799f417553208de1bc8023364
lleung030 <lucasleung2593@gmail.com>
2023-07-11 18:16:29 -0700

    hero page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Lucas' Portfolio</title>
       </Head>
       <Header />
-      <section id="hero">
+      <section id="hero" className='snap-center'>
       <Hero />
       </section>
     </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
 type Props = {};
 
@@ -9,19 +11,43 @@ function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
       "Hi, my Name is Lucas",
-      "I am a Software Developer",
-      "...Software Engineer",
-      "...Full Stack Developer",
+      "Welcome to my Portfolio",
+      "Enjoy your stay!",
     ],
     loop: true,
     delaySpeed: 2000,
   });
   return (
-    <div>
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F&AB0A" />
-      </h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://photos.google.com/photo/AF1QipOFe183972dx8LJyig-pWv8ycUPb_OJmXVgmZJ4"
+        alt=""
+      />
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F&AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroBtn">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroBtn">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroBtn">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroBtn">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
